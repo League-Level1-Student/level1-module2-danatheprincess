30d818407b1d9de8b128eea65281ef6074fff091
student <student@ip-172-31-59-75.us-west-2.compute.internal>
2021-07-06 18:36:52 -0700

    finish module 2

--- src/TeaParty.java
+++
@@ -1,7 +0,0 @@
-
-public class TeaParty {
-	    public String welcome(String name, boolean isWoman, boolean isKnighted) {
-	        
-	    }
-	}
-}
---
+++ src/_06_tea_party/TeaParty.java
@@ -0,0 +1,24 @@
+package _06_tea_party;
+
+public class TeaParty {
+	    public String welcome(String name, boolean isWoman, boolean isKnighted) {
+	        
+	    	String greeting="Hello ";
+	    if(isWoman) {
+	    	if(isKnighted) {
+	    		greeting+="Lady ";
+	    	}else {
+	    		greeting+="Ms. ";
+	    	}
+	    }else {
+	    	if(isKnighted) {
+	    		greeting+="Sir ";
+	    	}else {
+	    		greeting+="Mr. ";
+	    	}
+	    	
+	    }
+	    greeting+=name;
+	    return greeting;
+	}
+}
